70b157beb03b0c11d50fae28ac6e93f6d1d5027b
Nick Babcock <nbabcock19@hotmail.com>
2018-03-13 17:08:42 -0400

    Fix build script to make directory

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ yarn run parcel build --out-dir themes/pb/dist --public-url './' themes/pb/index
 sed -i -e 's|http://{|{|g' themes/pb/dist/index.html
 
 cp themes/pb/dist/index.html themes/pb/layouts/.
+mkdir -p themes/pb/static
 cp themes/pb/dist/* themes/pb/static/.
