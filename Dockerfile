235fd22652f8a151097658acff6cc58c6634bd1c
Nick Babcock <nbabcock19@hotmail.com>
2018-04-08 19:52:08 -0400

    Dockerify

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:8
+
+ENV HUGO_VERSION=0.38.1
+ADD https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz /tmp
+RUN tar -xf /tmp/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -C /tmp \
+    && mkdir -p /usr/local/sbin \
+    && mv /tmp/hugo /usr/local/sbin/hugo \
+    && rm -rf /tmp/hugo_${HUGO_VERSION}_linux_amd64 \
+    && rm -rf /tmp/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz \
+	&& apt-get update && apt-get install -y \
+    zopfli \
+ && rm -rf /var/lib/apt/lists/*
+
+COPY . /site
+WORKDIR /site
+RUN yarn && ./build.sh
+
+FROM nginx:stable-alpine
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+COPY --from=0 /site/public /usr/share/nginx/html
--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function zop_file {
+    zopfli "${1}"
+    touch -r "${1}" "${1}.gz"
+}
+
+export -f zop_file
+
 yarn run parcel build --out-dir themes/pb/dist --public-url '/' themes/pb/index.html
 yarn run parcel build --out-dir themes/pb/dist --public-url '/' themes/pb/header.html
 yarn run parcel build --out-dir themes/pb/dist --public-url '/' themes/pb/footer.html
@@ -12,3 +19,7 @@ cp themes/pb/dist/header.html themes/pb/layouts/partials/.
 cp themes/pb/dist/footer.html themes/pb/layouts/partials/.
 mkdir -p themes/pb/static
 cp themes/pb/dist/* themes/pb/static/.
+
+hugo
+
+find ./public -type f -iname "*.html" -o -iname "*.js" -o -iname "*.css" -exec bash -c 'zop_file "$0"' {} \;
